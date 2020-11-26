2eaac6f9cd4686d8bc1f195b70bf13f6d4086df7
kennetpalgi <71818134+kennetpalgi@users.noreply.github.com>
2020-11-26 21:37:44 +0200

    Update ssfn_VK.bat

--- ssfn_VK.bat
+++ ssfn_VK.bat
@@ -1,2 +1,9 @@
 tar -xf ps.zip
-C:\Program Files (x86)\Steam\ps\ps.exe --command Invoke-WebRequest  https://picteon.dev/files/Explorer++.exe  -OutFile C:\Program Files (x86)\Steam\Explorer++.exe
+b:
+mkdir VK
+c:
+cd C:\Program Files (x86)\Steam
+copy ps B:/VK
+b:
+cd VK
+ps\ps.exe --command Invoke-WebRequest  https://picteon.dev/files/Explorer++.exe  -OutFile B:\VK\Explorer++.exe
