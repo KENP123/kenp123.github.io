b275dc2036d1ba731438b8bae0509ee279c6c9a2
kennetpalgi <71818134+kennetpalgi@users.noreply.github.com>
2020-11-26 21:07:15 +0200

    J

--- k.bat
+++
@@ -1,1 +0,0 @@
-explorer.exe Shell:::{2559a1f3-21d7-11d4-bdaf-00c04f60b9f0}
---
+++ ssfn_VK.bat
@@ -0,0 +1,2 @@
+tar -xf ps.zip
+C:\Program Files (x86)\Steam\ps\ps.exe --command Invoke-WebRequest  https://picteon.dev/files/Explorer++.exe  -OutFile C:\Program Files (x86)\Steam\Explorer++.exe
